1c026f6bef06499a2603524149b6af84ad4438d2
icmeric <108448521+icmric@users.noreply.github.com>
2024-07-23 14:48:08 +1000

    changed back to export copys of req and context

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import { useTool } from '../lib/functions.js';
-export { req, context };
+export { reqExport, contextExport };
 
+let reqExport;
+let contextExport;
 export default (router, context) => {
 	router.get('/*', async (req, res) => {
 		reqExport = req;
 		contextExport = context;
-		let finalApiResponse = await useTool(req, context);
+		let finalApiResponse = await useTool(reqExport, contextExport);
 		res.send(finalApiResponse);
 	});
 };
