538ac6ea63665fa3c92d2a389ff2a51a66183fdd
icmeric <108448521+icmric@users.noreply.github.com>
2024-07-23 14:54:49 +1000

    added basic auth check

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,174 @@
-async function e(e,t){let a=e.url.split(/\/|\?/)[1],r=e.query;const{services:l,getSchema:n}=t,{ItemsService:i}=l,s=new i("api_parents",{schema:await n(),accountability:e.accountability});let u;try{u=await s.readByQuery({fields:["*","api.*"],filter:{title:{_eq:a}}})}catch(e){}if(""==u)return s.readByQuery({fields:["title"]});let o={};if(null!=u[0].extra_values)for(let e=0;e<u[0].extra_values.length;e++){let t=u[0].extra_values[e].key,a=u[0].extra_values[e].value;o[t]=a}let c=u[0].api,y=u[0];t.data={},t.data.reqAccountability=e.accountability,t.data.$url=r,t.data.$tool={},t.data.$tool.main=h(y.main);let f,d={request:null};return 0==(null==c.request||c.request=={})&&(d.request=h(c.request)),t.data.apiResponse=await async function(e,t){const{method:a,url:r,header:l}=e;let n,i={};null!=l&&l.forEach((e=>{i[e.header_title]=e.header_content}));0!=Object.keys(o).length&&(i=o);let s={method:e.method,headers:null!=i?i:null};null!=t&&(s.body=JSON.stringify(t));return n=await fetch(e.url,s).then((e=>e.json())),d.response=n,n}(c,d.request),f=null!=c.transform?await h(c.transform):t.data.apiResponse,f;function p(e){return e.replace(/{(.*)}/g,((e,a)=>{let r=("data."+a).trim().split(".").reduce(((e,t)=>e&&"object"==typeof e?e[t]:"Blank"),t);let l;return l="string"==typeof r?r:JSON.stringify(r),void 0!==l?l:e}))}function h(e){if("string"==typeof e){let t=p(e);try{return JSON.parse(t)}catch(e){return t}}else{if(Array.isArray(e))return e.map((e=>h(e)));if(null!==e&&"object"==typeof e){const t={};for(const a in e)e.hasOwnProperty(a)&&(t[a]=h(e[a]));return t}}return e}}let t,a;var r=(r,l)=>{r.get("/*",(async(r,n)=>{t=r,a=l;let i=await e(t,a);n.send(i)}))};export{a as contextExport,r as default,t as reqExport};
+async function useTool(req, context) {
+    const urlRequestsBreakdown = req.url.split(/\/|\?/);
+    let requestedTool = urlRequestsBreakdown[1];
+    let requestedToolQuery = req.query;
+
+    const { services, getSchema } = context;
+    const { ItemsService } = services;
+
+    const itemx = new ItemsService('api_parents', {
+        schema: await getSchema(),
+        accountability: req.accountability
+    });
+
+
+    let datax;
+    try {
+        datax = await itemx.readByQuery({ fields: ['*', 'api.*'], filter: { 'title': { '_eq': requestedTool } } });
+    } catch (e) { }
+
+    if (datax == "") {
+        return itemx.readByQuery({ fields: ['title'] });
+    }
+
+    // Gets extra values from parent if present
+    let extraValues = {};
+    if (datax[0].extra_values != null) {
+        for (let i = 0; i < datax[0].extra_values.length; i++) {
+            let key = datax[0].extra_values[i].key;
+            let value = datax[0].extra_values[i].value;
+            extraValues[key] = value;
+        }
+    }
+
+    let apiRequest = datax[0].api;
+    let tool = datax[0];
+
+    context.data = {};
+    context.data.reqAccountability = req.accountability;
+    context.data.$url = requestedToolQuery;
+    context.data.$tool = {};
+    context.data.$tool.main = recursiveReplace(tool.main);
+
+
+    let apiData = {
+        "request": null,
+    };
+    if ((apiRequest.request == null || apiRequest.request == {}) == false) {
+        apiData.request = recursiveReplace(apiRequest.request);
+    }
+
+    // Makes API call and saves raw response
+    context.data.apiResponse = await performApiCall(apiRequest, apiData.request);
+
+    let apiResponceObj;
+    if (apiRequest.transform != null) {
+        // Uses passes transform object to gather all data to return
+        apiResponceObj = await recursiveReplace(apiRequest.transform);
+    } else {
+        // If no transform object is provided, the raw api response is returned
+        apiResponceObj = context.data.apiResponse;
+    }
+
+    return apiResponceObj;
+
+    // Attempts to find a value at the given path. If no value is found, returns the path.
+    function resolvePath(replacementVariablePath) {
+        replacementVariablePath = "data." + replacementVariablePath;
+        return replacementVariablePath.trim().split('.').reduce((prev, curr) => {
+            if (prev && typeof prev === 'object') {
+                return prev[curr];
+            }
+            return "Blank";
+        }, context);
+    }
+
+    // Uses a regex to find all placeholders and run resolvePath() with them
+    function replaceInValue(value) {
+        let regex = /{(.*)}/g;
+        return value.replace(regex, (match, p1) => {
+            let returnValue = resolvePath(p1);
+            let resolved;
+            if (typeof returnValue === "string") {
+                resolved = returnValue;
+            } else {
+                // if the result is not a string, turn it into a string
+                resolved = JSON.stringify(returnValue);
+            }
+            return resolved !== undefined ? resolved : match;
+        });
+    }
+
+    function recursiveReplace(rawObject) {
+        if (typeof rawObject === 'string') {
+            // End recursion, run replaceInValue() to find and replace placeholders
+            let updatedValue = replaceInValue(rawObject);
+            try {
+                // attempts to convert to and return a JSON version of the updated value
+                return JSON.parse(updatedValue);
+            } catch (e) {
+                // Will fail if the updated value is not JSON or Array, returns updatedValue as is
+                return updatedValue;
+            }
+        } else if (Array.isArray(rawObject)) {
+            // Recurses again if the value is an array for each item in the array
+            return rawObject.map(item => recursiveReplace(item));
+        } else if (rawObject !== null && typeof rawObject === 'object') {
+            // Recurses again if the value is an object for each value in the object
+            const result = {};
+            for (const key in rawObject) {
+                if (rawObject.hasOwnProperty(key)) {
+                    result[key] = recursiveReplace(rawObject[key]);
+                }
+            }
+            // result of the recursed object
+            return result;
+        }
+        // Catch all
+        return rawObject;
+    }
+
+    async function performApiCall(apiCallDetails, apiCallBody) {
+        // get the necessary details from apiCallDetails
+        const { method, url, header } = apiCallDetails;
+        let headers = {};
+
+        if (header != null) {
+            // Build headers object from the header array
+            header.forEach(h => {
+                headers[h.header_title] = h.header_content;
+            });
+        }
+
+
+        // checks if extra values were passed, if so, uses them as headers
+        if (Object.keys(extraValues).length != 0) {
+            headers = extraValues;
+        }
+
+
+        let apiResponse;
+        let apiRequestInfo = {
+            method: apiCallDetails.method,
+            headers: headers != null ? headers : null
+        };
+        if (apiCallBody != null) {
+            apiRequestInfo.body = JSON.stringify(apiCallBody);
+        }
+        // Execute the fetch request with the provided details and capture the response
+        apiResponse = await fetch(apiCallDetails.url, apiRequestInfo).then(response => { return response.json() });
+        apiData.response = apiResponse;
+        return apiResponse;
+    }
+}
+
+let reqExport;
+let contextExport;
+
+var index = (router, context) => {
+	router.get('/*', async (req, res) => {
+		// dodgy way of checking auth, should compare against list of users
+		// to use perm level rather than user, use .admin instead of .user 
+		if (req.accountability.user != null) {
+			reqExport = req;
+			contextExport = context;
+			let finalApiResponse = await useTool(reqExport, contextExport);
+			res.send(finalApiResponse);
+		} else {
+			// Can also be sent as plain string
+			res.send({'Request failed': 'Unauthorized, Please log in!'});
+		}
+	});
+};
+
+export { contextExport, index as default, reqExport };
--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ export { reqExport, contextExport };
 
 let reqExport;
 let contextExport;
+
 export default (router, context) => {
 	router.get('/*', async (req, res) => {
-		reqExport = req;
-		contextExport = context;
-		let finalApiResponse = await useTool(reqExport, contextExport);
-		res.send(finalApiResponse);
+		// dodgy way of checking auth, should compare against list of users
+		// to use perm level rather than user, use .admin instead of .user 
+		if (req.accountability.user != null) {
+			reqExport = req;
+			contextExport = context;
+			let finalApiResponse = await useTool(reqExport, contextExport);
+			res.send(finalApiResponse);
+		} else {
+			// Can also be sent as plain string
+			res.send({'Request failed': 'Unauthorized, Please log in!'});
+		}
 	});
 };
